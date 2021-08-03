6940cd6f3315dfbfcd74ab1acdd46f83769e5ac0
Mosquito <Mosquitod3@yandex.ru>
2021-08-03 19:03:14 +0300

    Scriplet. Динамическая таблица.

---
+++ src/main/java/ru/jobj/dream/store/Store.java
@@ -0,0 +1,27 @@
+package ru.jobj.dream.store;
+
+import ru.jobj.dream.model.Post;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Store {
+    private static final Store INST = new Store();
+
+    private final Map<Integer, Post> posts = new HashMap<>();
+
+    private Store() {
+        posts.put(1, new Post(1, "Junior java job"));
+        posts.put(2, new Post(2, "Middle java job"));
+        posts.put(3, new Post(3, "Senior java job"));
+    }
+
+    public static Store instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
