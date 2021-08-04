00e91564bbd8ae5c7ab42cd22b19c1d709baf701
Mosquito <Mosquitod3@yandex.ru>
2021-08-04 22:31:43 +0300

    Редактирование вакансии.

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -41,12 +41,18 @@ public class Store {
    }
 
    public void save(Post post) {
-        post.setId(POST_ID.incrementAndGet());
-        posts.put(post.getId(), post);
+       if (post.getId() == 0) {
+           post.setId(POST_ID.incrementAndGet());
+       }
+       posts.put(post.getId(), post);
    }
 
    public void save(Candidate candidate) {
         candidate.setId(CANDIDATE_ID.incrementAndGet());
         candidates.put(candidate.getId(), candidate);
    }
+
+    public Post findById(int id) {
+        return posts.get(id);
+    }
 }
