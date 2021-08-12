bb6437d1253c72ed764b9abe5fe21c652d7ef8e5
Mosquito <Mosquitod3@yandex.ru>
2021-08-13 01:28:09 +0300

    Расширить Store для пользователя

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -2,6 +2,7 @@ package ru.job4j.dream.store;
 
 import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
+import ru.job4j.dream.model.User;
 
 import java.util.Collection;
 
@@ -10,13 +11,19 @@ public interface Store {
 
     Collection<Candidate> findAllCandidates();
 
+    Collection<User> findAllUsers();
+
     void save(Post post);
 
     void save(Candidate candidate);
 
+    void save(User user);
+
     Post findByIdPost(int id);
 
     Candidate findByIdCandidate(int id);
+
+    User findByIdUser(int id);
 
     void removeCandidate(int id );
 }
